1271cca5f1222d590a6faeb631469f0ec4e7ac45
github username <github email address>
2019-05-17 14:53:38 -0600

    Add toggle function to display additional info

--- src/components/HogCard.js
+++ src/components/HogCard.js
@@ -1,26 +1,38 @@
 import React from 'react'
 import HiddenFields from './HiddenFields'
 
+function additionalInfo(props) {
+  console.log('fired')
+  if (!props.deetz) {
+    return(
+
+        "Specialty:" + `${props.specialty}` +
+        "Weight:" + `${props['weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water']}\n` +
+        "Highest Medal:" + `${props['highest medal achieved']}`
+    );
+  }
+  return(
+    null
+  );
+}
+
 class HogCard extends React.Component{
   constructor(props){
     super(props)
-    this.state={
-      buttonClicked: false
-    }
+    this.state={buttonClicked: false};
+    this.changeState = this.changeState.bind(this);
   }
 
-  renderHiddenFields = (props) =>{
-    return(
-      <div className="deetz">
-        <h2>Specialty:{this.props.specialty}</h2>
-        <h2>Weight:{this.props['weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water']}</h2>
-        <h2>Highest Medal:{this.props['highest medal achieved']}</h2>
-      </div>
-    )
-  }
+  // renderHiddenFields = (props) =>{
+  //   return(
+  //
+  //   );
+  // }
 
-  changeState = () => {
-    this.setState({buttonClicked: true})
+  changeState() {
+    this.setState(state => ({
+      buttonClicked: !state.buttonClicked
+    }));
   }
 
     render(){
@@ -29,8 +41,10 @@ class HogCard extends React.Component{
               <h1>{this.props.name}</h1>
               <img src={this.props.findHogImg(this.props.name)} className="hog-image" alt={this.props.name}/>
               <h4>{this.props.greased}</h4>
-              <button onClick={this.changeState}>More Info!</button>
-              <h4>{this.state.buttonClicked ? this.renderHiddenFields : console.log("false")}</h4>
+              <div>
+                <additionalInfo  />
+                <button onClick={this.changeState}>{this.state.buttonClicked ? `${additionalInfo(this.props)}` : 'More Info'}</button>
+              </div>
           </div>
       )
     }
