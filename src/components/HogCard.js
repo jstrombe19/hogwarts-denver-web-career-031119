193d6f74be24fe909c251d04eca402d7e9caded6
github username <github email address>
2019-05-17 09:56:05 -0600

    console logged props

--- src/components/HogCard.js
+++ src/components/HogCard.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 const HogCard = (props) => {
+    console.log(props)
     return(
         <div>
             <h1>{props.name}</h1>
-            <h2>{props.specialty}</h2>
-            <h3>{props.greased}</h3>
+            <h4>{props.specialty}</h4>
+            <h4>{props.greased}</h4>
         </div>
     )
 }
